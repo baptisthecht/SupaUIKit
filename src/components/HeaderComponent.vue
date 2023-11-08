b8b9cdb3ef42d2f094b3d4e73390282924de4a02
Baptist HECHT <me@baptisthecht.fr>
2023-11-08 02:08:42 +0100

    improved responsivity

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,19 +1,26 @@
 <template>
-    <header class="flex justify-between items-center py-7 mx-auto max-w-[1300px]">
+    <header class="flex justify-between items-center p-8 mx-auto max-w-[1300px]">
         <div class="flex items-center gap-4">
             <img src="Avatar.png" alt="" class="w-8 h-8">
             <h1 class="font-semibold text-xl text-dark tracking-tight">Baptist Hecht Designs</h1>
         </div>
         <div>
-            <ul class="flex gap-10 font-medium text-dark">
+            <ul class="hidden md:flex gap-10 font-medium text-dark">
                 <li><a href="/components">Components</a></li>
                 <li><a href="/templates">Templates</a></li>
                 <li><a href="/docs">Docs</a></li>
             </ul>
         </div>
-        <svg @click="showSearchModal" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 cursor-pointer text-dark">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
+        <div class="flex gap-5">
+            <svg @click="showSearchModal" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 cursor-pointer text-dark">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 cursor-pointer text-dark block md:hidden">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+        </div>
+
+          
     </header>
     <SearchModal/>
 </template>
