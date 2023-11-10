26fb208327d871f19daa93ba380e252d90037f35
Baptist HECHT <me@baptisthecht.fr>
2023-11-10 20:33:42 +0100

    FIX: Fixed Read the docs button for mobile

--- src/components/WelcomeContentComponent.vue
+++ src/components/WelcomeContentComponent.vue
@@ -28,7 +28,10 @@
             </a>  
             <a href="/docs" class="font-medium text-lg flex items-center gap-3 text-darker group">
                 Read the docs 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-4 h-4 -translate-x-3 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition ease-in-out duration-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-4 h-4 -translate-x-3 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition ease-in-out duration-100 hidden lg:block">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-4 h-4 block lg:hidden">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                 </svg>
             </a>
